0da431f2c2ed76c126d00b2bf5d0994359c3450e
Alex Fey <alex.loofet@gmail.com>
2017-11-23 16:33:30 +0100

    Add startside

--- src/client/components/Login.js
+++ src/client/components/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const scopes = [
   'user-read-private',
@@ -29,6 +30,9 @@ const width = 450,
   left = screen.width / 2 - width / 2,
   top = screen.height / 2 - height / 2
 class Login extends Component {
+  state = {
+    navClassName: 'navbar-start'
+  }
   componentDidMount() {
     window.addEventListener('message', e => this.auth(e), false)
   }
@@ -43,7 +47,208 @@ class Login extends Component {
 
     return (
       <div className="login-wrapper">
-        <a
+        <div>
+          <i
+            className="fa fa-angle-double-up"
+            id="scrollToTop"
+            aria-hidden="true"
+          />
+          <nav className="navbar ">
+            <div className="navbar-brand">
+              <NavLink className="navbar-item" to="/">
+                <img
+                  src={require('../images/Soundize-logo2.png')}
+                  alt="asasas"
+                />
+              </NavLink>
+
+              <a
+                className="navbar-item is-hidden-desktop"
+                href="https://github.com/jgthms/bulma"
+                target="_blank"
+              >
+                <span className="icon">
+                  <i className="fa fa-github" />
+                </span>
+              </a>
+
+              <a
+                className="navbar-item is-hidden-desktop"
+                href="https://twitter.com/jgthms"
+                target="_blank"
+              >
+                <span className="icon">
+                  <i className="fa fa-twitter" />
+                </span>
+              </a>
+
+              <div
+                className="navbar-burger burger"
+                data-target="navMenubd-example"
+              >
+                <span />
+                <span />
+                <span />
+              </div>
+            </div>
+
+            <div id="navMenubd-example" className="navbar-menu">
+              <div className="navbar-end">
+                <a className="login-btn">
+                  <span>Logga in</span>
+                </a>
+              </div>
+            </div>
+          </nav>
+          <section className="wrapper">
+            <div className="banner-info">
+              <h1>Discover a universe of music</h1>
+              <h1>Login now!</h1>
+              <p>
+                <a className="banner-button" href="#">
+                  LEARN MORE
+                </a>
+              </p>
+              <p>
+                <a className="scroll-down" id="mini-info" href="#">
+                  Learn about Soundize
+                </a>
+              </p>
+              <br />
+              <i
+                className="fa fa-arrow-down scroll-down bounce"
+                aria-hidden="true"
+              />
+            </div>
+          </section>
+          <section className="banner-two">
+            <img src={require('../images/devices.png')} alt="devices" />
+            <div className="banner-two-info">
+              <h1 id="slide-one">What's on Soundize?</h1>
+              <h2 id="slide-two">Music</h2>
+              <p id="slide-two">
+                There are millions of songs on Maesto. Play your favorites,
+                discover new tracks, and build the perfect collection.
+              </p>
+              <h2 id="slide-three">Playlists</h2>
+              <p id="slide-three">
+                You’ll find readymade playlists to match your mood, put together
+                by music fans and experts.
+              </p>
+              <h2 id="slide-four">New releases</h2>
+              <p id="slide-four">
+                Hear the week's latest singles and albums, and check out what's
+                hot in the Top 50.
+              </p>
+            </div>
+          </section>
+          <section className="banner-three">
+            <i
+              className="fa fa-arrow-down scroll-down-two"
+              aria-hidden="true"
+            />
+            <div className="carousel wrapper">
+              <div>
+                <h1>Music for everyone.</h1>
+                <p>
+                  <a className="banner-button" href="#">
+                    GET MAESTRO FREE
+                  </a>
+                  <a className="banner-button" href="#">
+                    GEAT MAESTRO PREMIUM
+                  </a>
+                </p>
+              </div>
+              <div>
+                <h1>Premium for your whole family.</h1>
+                <p>
+                  <a className="banner-button" href="#">
+                    GET MAESTRO PREMIUM
+                  </a>
+                </p>
+              </div>
+              <div>
+                <h1>2017's Top 50 out now.</h1>
+                <p>
+                  <a className="banner-button" href="#">
+                    CHECK IT OUT
+                  </a>
+                </p>
+              </div>
+            </div>
+          </section>
+          <section className="banner-four">
+            <i
+              className="fa fa-arrow-down scroll-down-three"
+              aria-hidden="true"
+            />
+            <div className="service-header">
+              <h1>One account. Listen everywhere.</h1>
+              <p>Mobile - Computer - Tablet - Web player - Car</p>
+            </div>
+            <div className="services">
+              <div className="service-one">
+                <h2>Search</h2>
+                <p>
+                  Know what you want to listen to? Just search and hit play.
+                </p>
+              </div>
+              <div className="service-two">
+                <h2>Browse</h2>
+                <p>
+                  Check out the latest charts, brand new releases and great
+                  playlists for right now.
+                </p>
+              </div>
+              <div className="service-three">
+                <h2>Discover</h2>
+                <p>
+                  Enjoy new music every Monday with your own personal playlist.
+                  Or sit back and enjoy Radio.
+                </p>
+              </div>
+            </div>
+          </section>
+          <div>
+            <i
+              className="fa fa-arrow-down scroll-down-four"
+              aria-hidden="true"
+            />
+          </div>
+          <section className="banner-five">
+            <div className="pictures-one">
+              <img alt="" />
+              <img alt="" />
+              <img alt="" />
+            </div>
+            <div className="pictures-two">
+              <img alt="" />
+              <img alt="" />
+              <img alt="" />
+            </div>
+          </section>
+          <footer>
+            <div id="footer">
+              <span className="slide">
+                <i
+                  className="fa fa-facebook-square footicon"
+                  aria-hidden="true"
+                />
+                <i
+                  className="fa fa-linkedin-square footicon"
+                  aria-hidden="true"
+                />
+                <i className="fa fa-instagram footicon" aria-hidden="true" />
+                <i
+                  className="fa fa-twitter-square footicon"
+                  aria-hidden="true"
+                />
+              </span>
+              <h2>Copyright - 2017 Maestro AB</h2>
+            </div>
+          </footer>
+        </div>
+        {/* <a
           onClick={() =>
             window.open(
               url,
@@ -60,7 +265,7 @@ class Login extends Component {
           className="button login-button"
         >
           ENTER
-        </a>
+        </a> */}
       </div>
     )
   }

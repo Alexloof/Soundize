0de3a14f82d6749b584170ea9d9e7fd32fe6da9f
Alex Fey <alex.loofet@gmail.com>
2017-12-03 20:58:18 +0100

    cleaned up

--- server.js
+++ server.js
@@ -5,24 +5,12 @@ import Routes from './src/routes'
 import renderer from './src/helpers/renderer'
 import createStore from './src/helpers/serverStore'
 
-// Since the root/src dir contains our index.html
-//app.use(express.static(__dirname + '/public/'))
-
-// Heroku bydefault set an ENV variable called PORT=443
-//  so that you can access your site with https default port.
-// Falback port will be 8080; basically for pre-production test in localhost
-// You will use $ npm run prod for this
-//app.listen(process.env.PORT || 8080)
-
 const app = express()
 
 app.use(express.static('public'))
 
 app.get('*', (req, res) => {
   const store = createStore(req)
-  // const context = {}
-  // const content = renderer(req, store, context)
-  // res.send(content)
 
   const promises = matchRoutes(Routes, req.path)
     .map(({ route }) => {
